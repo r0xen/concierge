af2594e6cce10a3491e2a8e376d27c838b974089
Andrea <andrea.basi@icloud.com>
2019-04-13 04:03:45 +0200

    fix nullcomand

--- src/Concierge/Commands/NullCommand.php
+++ src/Concierge/Commands/NullCommand.php
@@ -6,4 +6,9 @@ namespace Concierge\Commands;
  * Null command which does nothing!
  */
 class NullCommand extends CommandAbstract
-{ }
+{ 
+    public function execute(): void
+    {
+        return;
+    }
+}
