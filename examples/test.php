02beb81462ea70b23c2bfab3e3ee5969c8ce09e8
Andrea <andrea.basi@icloud.com>
2019-04-15 07:51:55 +0200

    updated examples

--- examples/test.php
+++ examples/test.php
@@ -1,24 +1,49 @@
 <?php
 declare(strict_types = 1);
 
-include __DIR__.'/basics.php';
+include __DIR__.'/../vendor/autoload.php';
 use Concierge\Concierge;
 
-$mc = new Concierge();
+define('BOT_TOKEN', 'BOT_TOKEN');
+
+define('A_USER_CHAT_ID', 'BOT_OWNER_CHAT_ID');
+
+define('IG_USERNAME', 'FIRST_ACCOUNT_USERNAME');
+
+define('IG_PASSWORD', 'FIRST_ACCOUNT_PASSWORD');
+
 
 $ig = new \InstagramAPI\Instagram(false, false);
         try {
-            $loginResponse = $ig->login("SECOND_ACCOUNT", "IG_PASSWORD");
+            $loginResponse = $ig->login(IG_USERNAME, IG_PASSWORD);
             if ($loginResponse !== null && $loginResponse->isTwoFactorRequired()) {
                 $twoFactorIdentifier = $loginResponse->getTwoFactorInfo()->getTwoFactorIdentifier();
                 $verificationCode = trim(fgets(STDIN));
-                $ig->finishTwoFactorLogin("SECOND_ACCOUNT", "IG_PASSWORD", $twoFactorIdentifier, $verificationCode);
+                $ig->finishTwoFactorLogin(IG_USERNAME, IG_PASSWORD, $twoFactorIdentifier, $verificationCode);
             }
         } catch (\Exception $e) {
             echo 'Something went wrong: ' . $e->getMessage() . "\n";
             exit(0);
         }
 
-$mc->addInstagram("ig2", $ig);
+$mc = new Concierge(IG_USERNAME, $ig);
 
+/*
+//multi account
+$ig2 = new \InstagramAPI\Instagram(false, false);
+        try {
+            $loginResponse = $ig->login("username", "password");
+            if ($loginResponse !== null && $loginResponse->isTwoFactorRequired()) {
+                $twoFactorIdentifier = $loginResponse->getTwoFactorInfo()->getTwoFactorIdentifier();
+                $verificationCode = trim(fgets(STDIN));
+                $ig->finishTwoFactorLogin("username", "password", $twoFactorIdentifier, $verificationCode);
+            }
+        } catch (\Exception $e) {
+            echo 'Something went wrong: ' . $e->getMessage() . "\n";
+            exit(0);
+        }
+
+
+$mc->addInstagram("second_account", $ig2);
+*/
 $mc->run();
